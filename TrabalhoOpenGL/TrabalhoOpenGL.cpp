afdd90e6f9cab7c53695207ae2a839900d553b50
Thriliriel <wsk.shadow@gmail.com>
2017-03-16 22:03:30 -0300

    Terminei o escalamento da cena.

--- TrabalhoOpenGL/TrabalhoOpenGL.cpp
+++ TrabalhoOpenGL/TrabalhoOpenGL.cpp
@@ -4,6 +4,8 @@
 #include "stdafx.h"
 #include <stdlib.h>
 #include "gl\glut.h"
+#include <time.h>
+#include <iostream>
 
 using namespace std;
 
@@ -12,6 +14,11 @@ const int MAP_SIZE = 10;
 
 //escala do mapa (default 1)
 int scale = 1;
+
+int Fps = 0;
+float LastTickCount = 0.0f;
+float CurrentTickCount;
+char FrameRate[50] = "";
 
 //array de disposição do mapa
 //0 = cubinhos
@@ -67,6 +74,13 @@ void reshape(int w, int h)
 	);
 }
 
+void changeCamera(int x, int y) {
+	// Especifica posição do observador e do alvo
+	gluLookAt(0, 0, 0, 0, 0, 0, 0, 0, 0);
+
+	glutPostRedisplay();
+}
+
 // **********************************************************************
 //  void display( void )
 //
@@ -95,10 +109,24 @@ void display(void)
 
 	glLineWidth(3);
 
-	for (int j = 0; j < MAP_SIZE; j++) {
-		for (int i = 0; i < MAP_SIZE; i++) {
+	int index = 0;
+	int indexJ = 0;
+	int indexI = 0;
+	int indexControlJ = 0;
+	int indexControlI = 0;
+	for (int j = 0; j < MAP_SIZE*scale; j++) {
+		indexI = 0;
+
+		if (indexControlJ == 0) {
 			//se map nessa posição for 0, desenha
-			int index = (j * MAP_SIZE) + i;
+			index = (indexJ * MAP_SIZE) + indexI;
+		}
+
+		for (int i = 0; i < MAP_SIZE*scale; i++) {
+			if (indexControlI == 0) {
+				//se map nessa posição for 0, desenha
+				index = (indexJ * MAP_SIZE) + indexI;
+			}
 
 			if (map[index] == 0) {
 				glBegin(GL_QUADS);
@@ -121,10 +149,36 @@ void display(void)
 
 				glEnd();
 			}
+
+			if (indexControlI < scale-1) {
+				indexControlI++;
+			}
+			else {
+				indexControlI = 0;
+				indexI++;
+			}
+		}
+		
+		if (indexControlJ < scale-1) {
+			indexControlJ++;
+		}
+		else {
+			indexControlJ = 0;
+			indexJ++;
 		}
 	}
 
 	glutSwapBuffers();
+
+	/*CurrentTickCount = clock() * 0.001f;
+	Fps++;
+
+	if ((CurrentTickCount - LastTickCount) > 1.0f)
+	{
+		LastTickCount = CurrentTickCount;
+		std::cout << (FrameRate, "[ FPS: %d ]", Fps);
+		Fps = 0;
+	}*/
 }
 
 
@@ -141,7 +195,8 @@ void keyboard(unsigned char key, int x, int y)
 		case 27:        // Termina o programa qdo
 			exit(0);   // a tecla ESC for pressionada
 			break;
-
+		case 119: //w
+			changeCamera(10, 0);
 		default:
 			break;
 	}
@@ -191,7 +246,6 @@ int  main(int argc, char** argv)
 	// executa algumas inicializações
 	init();
 
-
 	// Define que o tratador de evento para
 	// o redesenho da tela. A funcao "display"
 	// será chamada automaticamente quando
@@ -223,3 +277,102 @@ int  main(int argc, char** argv)
 
 	return 0;
 }
+
+/*
+// TeaPot3D.c - Isabel H. Manssour
+// Um programa OpenGL que exemplifica a visualização 
+// de objetos 3D.
+// Este código está baseado nos exemplos disponíveis no livro 
+// "OpenGL SuperBible", 2nd Edition, de Richard S. e Wright Jr.
+
+
+GLfloat angle, fAspect;
+
+// Função callback chamada para fazer o desenho
+void Desenha(void)
+{
+	glClear(GL_COLOR_BUFFER_BIT);
+
+	glColor3f(0.0f, 0.0f, 1.0f);
+
+	// Desenha o teapot com a cor corrente (wire-frame)
+	glutWireTeapot(50.0f);
+
+	// Executa os comandos OpenGL
+	glutSwapBuffers();
+}
+
+// Inicializa parâmetros de rendering
+void Inicializa(void)
+{
+	glClearColor(0.0f, 0.0f, 0.0f, 1.0f);
+	angle = 45;
+}
+
+// Função usada para especificar a posição do observador virtual
+void PosicionaObservador(void)
+{
+	// Especifica sistema de coordenadas do modelo
+	glMatrixMode(GL_MODELVIEW);
+	// Inicializa sistema de coordenadas do modelo
+	glLoadIdentity();
+	// Especifica posição do observador e do alvo
+	gluLookAt(0, 80, 200, 0, 0, 0, 0, 1, 0);
+}
+
+// Função usada para especificar o volume de visualização
+void EspecificaParametrosVisualizacao(void)
+{
+	// Especifica sistema de coordenadas de projeção
+	glMatrixMode(GL_PROJECTION);
+	// Inicializa sistema de coordenadas de projeção
+	glLoadIdentity();
+
+	// Especifica a projeção perspectiva(angulo,aspecto,zMin,zMax)
+	gluPerspective(angle, fAspect, 0.5, 500);
+
+	PosicionaObservador();
+}
+
+// Função callback chamada quando o tamanho da janela é alterado 
+void AlteraTamanhoJanela(GLsizei w, GLsizei h)
+{
+	// Para previnir uma divisão por zero
+	if (h == 0) h = 1;
+
+	// Especifica o tamanho da viewport
+	glViewport(0, 0, w, h);
+
+	// Calcula a correção de aspecto
+	fAspect = (GLfloat)w / (GLfloat)h;
+
+	EspecificaParametrosVisualizacao();
+}
+
+// Função callback chamada para gerenciar eventos do mouse
+void GerenciaMouse(int button, int state, int x, int y)
+{
+	if (button == GLUT_LEFT_BUTTON)
+		if (state == GLUT_DOWN) {  // Zoom-in
+			if (angle >= 10) angle -= 5;
+		}
+	if (button == GLUT_RIGHT_BUTTON)
+		if (state == GLUT_DOWN) {  // Zoom-out
+			if (angle <= 130) angle += 5;
+		}
+	EspecificaParametrosVisualizacao();
+	glutPostRedisplay();
+}
+
+// Programa Principal
+int main(void)
+{
+	glutInitDisplayMode(GLUT_DOUBLE | GLUT_RGB);
+	glutInitWindowSize(350, 300);
+	glutCreateWindow("Visualizacao 3D");
+	glutDisplayFunc(Desenha);
+	glutReshapeFunc(AlteraTamanhoJanela);
+	glutMouseFunc(GerenciaMouse);
+	Inicializa();
+	glutMainLoop();
+}*/
